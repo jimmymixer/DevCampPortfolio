b22600065fd3b9324f09ea76916fb517df5ccbfc
jimmymixer <jaepark75@gmail.com>
2018-02-26 19:38:33 -0500

    Integrated nested attributes or portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,13 +1,18 @@
 class Portfolio < ApplicationRecord
   # Models should handle your logic instead of your controller
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs ['name'].blank? }
+
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+
   # Custom Scope 1
   # Can use this in the portfolios controller
   def self.angular
     where(subtitle: 'Angular')
   end
+
   # Custom Scope 2
   # Can use this in the portfolios controller
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
--- cheatsheet.rb
+++ cheatsheet.rb
@@ -44,3 +44,29 @@ in db/migrate/ the last migration file
     ... add default: 0 to the end of the line, separted with a comma
   end
   rails db:migrate
+
+Nested attributes
+  in portfolio.rb Model
+    add
+      accepts_nested_attributes_for :technologies,
+                                    reject_if: lambda { |attrs| attrs ['name'].blank? }
+  in rails console
+    Portfolio.create!(
+      title: "web App",
+      subtitle: "asdfasdf",
+      body: "asdfasfwew",
+      technologies_attributes: [
+        {name: "Ruby"},
+        {name: "Rails"},
+        {name: "angular"},
+        {name: "ionic"}
+      ]
+    )
+
+
+
+
+
+
+
+#
