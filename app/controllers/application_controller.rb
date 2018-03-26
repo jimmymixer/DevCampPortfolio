a4e24c1ba5ba8cdcd6a5d725161fae3dc2bb8435
jimmymixer <jaepark75@gmail.com>
2018-03-26 18:16:05 -0400

    Gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # Things in ApplicationController should be data specific
 
   # DeviseWhitelist is in controllers/concerns
   # DeviseWhitelist is for adding new table column in this case :name
@@ -14,4 +15,20 @@ class ApplicationController < ActionController::Base
   # Dynamic PageTitle
   include DefaultPageContent
 
+  # Build your own gem
+  before_action :set_copyright
+
+  # Build your own gem
+  def set_copyright
+    @copyright = ParkViewTool::Renderer.copyright 'Jae Park', 'All rights reserved'
+  end
+
+end
+
+module ParkViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
