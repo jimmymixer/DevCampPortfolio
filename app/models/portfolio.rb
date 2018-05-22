323cbe184c6a6786ff4f0155e0928ad4d6afd465
jimmymixer <jaepark75@gmail.com>
2018-05-21 22:33:01 -0400

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,8 +4,7 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs ['name'].blank? }
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   # Setup mapping between in uploader and database
   # mount_uploader tells portfolio.rb that it needs to call carrierwave
@@ -27,18 +26,5 @@ class Portfolio < ApplicationRecord
   # Custom Scope 2
   # Can use this in the portfolios controller
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-
-  # Callback
-  after_initialize :set_defaults
-
-  def set_defaults
-    # self.main_image ||= "http://via.placeholder.com/600x200"
-    # self.thumb_image ||= "http://via.placeholder.com/350x200"
-    # Placeholder used for developement and not for production
-    # Just to have something there to see the design of page
-    # Placeholder will be moved to be used as a view helper
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '200')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 
 end
