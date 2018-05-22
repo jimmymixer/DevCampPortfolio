f77e5cefdc4e04d8e5e5a4654ed61d34246e16e2
jimmymixer <jaepark75@gmail.com>
2018-05-21 21:34:53 -0400

    Finished carrierwave installation

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,6 +1,8 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
   storage :file
+  # If conneted to AWS use..
+  # stoage :aws
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
