19cfc03481be5be89e18085034a055d030acb7cc
jimmymixer <jaepark75@gmail.com>
2018-06-20 21:38:59 -0400

    Added comment notes for action cable.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Commented out "resources :comments" becauce of ActionCable
+  # resources :comments
+  
   # Added custom route for sign_in, sign_out, sign_up
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
@@ -17,6 +20,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
 
+  # This makes a websocket connection
   mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
